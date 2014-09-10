bd51a866d8218a528d04251dda557b82d509d6d7
mikey <mikey@mikeys-MacBook-Pro.local>
2014-09-09 20:02:41 -0700

    core functionality complete

--- app.js
+++ app.js
@@ -1,28 +1,196 @@
+var questionSet = [
+	question1 = {
+		question : 'What is the name of the famous bridge in San Francisco part?',
+		answer1 : 'sdfsdf',
+		answer2 : 'sdfds',
+		answer3 : 'Golden Gate Bridge',
+		answer4 : 'sdfsdf',
+		answer5 : 'sfsd'
+	},
+	question2 = {
+		question : 'What is the name of curviest street in the world?',
+		answer1 : "sdfsdf",
+		answer2 : "Lombard St.",
+		answer3 : 'sdfsdfsd',
+		answer4 : 'sdfsdf',
+		answer5 : 'sfsd'
+	},
+	question3 = {
+		question: 'What corner did the Greatful Dead make famous?',
+		answer1 : "Haight & Ashbury",
+		answer2 : "sdfds",
+		answer3 : 'sdfsdfsd',
+		answer4 : 'sdfsdf',
+		answer5 : 'sfsd'
+	},
+	question4 = {
+		question: 'What is the tallest building in SF?',
+		answer1 : "sdfsdf",
+		answer2 : "sdfds",
+		answer3 : 'sdfsdfsd',
+		answer4 : 'sdfsdf',
+		answer5 : 'Transamerica Building'
+	},
+	question5 = {
+		question: 'What famous US prison is located on an island?',
+		answer1 : "Alcatraz",
+		answer2 : "sdfds",
+		answer3 : 'sdfsdfsd',
+		answer4 : 'sdfsdf',
+		answer5 : 'sfsd'
+	}
+
+] 
+
+var correctAnswers = ['Golden Gate Bridge', "Lombard St.", "Haight & Ashbury", "Transamerica Building", "Alcatraz"];
+
+var userScore = 0;
+
+var questionNumber = 0;
+
+var questionNumberDisplay = 1;
+
 $(document).ready(function() {
 
 
 /* click on start game button to display question 1 with submit button */
 $('.startGame').on('click', "button", function() {
+	/* hide intro screen */
 	$('.startGame').hide();
-	$(this).closest('.container').find('.question1').show();
+	/* display template for question */
+	$(this).closest('.container').find('.questionGeneral').show();
+	/* question 1 questions and answers */
+	$(this).closest('.container').find('.questionGeneral').find('.question').text(questionSet[questionNumber].question);
+	$(this).closest('.container').find('.questionGeneral').find('label[for=answer1]').text(questionSet[questionNumber].answer1);
+	$(this).closest('.container').find('.questionGeneral').find('label[for=answer2]').text(questionSet[questionNumber].answer2);
+	$(this).closest('.container').find('.questionGeneral').find('label[for=answer3]').text(questionSet[questionNumber].answer3);
+	$(this).closest('.container').find('.questionGeneral').find('label[for=answer4]').text(questionSet[questionNumber].answer4);
+	$(this).closest('.container').find('.questionGeneral').find('label[for=answer5]').text(questionSet[questionNumber].answer5);
+	/* Display submit button */
 	$(this).closest('.container').find('.submit').show();
+
+
+	/* add question number to questionNumber text */
+	$(this).closest('.container').find('.questionGeneral').find('.questionNumber').text(questionNumberDisplay + '/5');
 
 
 })
 
 
 $('.questionGeneral').on('click', '.submit', function() {
-	if($('input:radio:checked').length > 0){
-		// go on with script
+	if($('input[name="question"]:checked').length > 0){
+		
+	
+
+
+		/* hide the submit button */
+		$(this).hide();
+
+		console.log(questionNumber)
+		/* if the user submits the correct answer */
+		if($('input[type="radio"]:checked').next('label').text() == correctAnswers[questionNumber]) {
+			/* increment user score for correct answer */
+			userScore++;
+
+			/* display correct for user */
+			$(this).closest('.questionGeneral').find('.correct').show();
+
+			/*display next button */
+			$(this).closest('.questionGeneral').find('.next').show();
+
+
+			console.log('correct');
+			console.log(userScore);
+		}
+		else {
+
+			/* display incorrect for user */
+			$(this).closest('.questionGeneral').find('.incorrect').show();
+
+			/*display next button */
+			$(this).closest('.questionGeneral').find('.next').show();
+
+			console.log("wrong!")
+		}
  	}
+
  	else{
     	alert("Please select an answer.");
  	}
+ 	/* increment questionNumber to 1 */
+	questionNumber ++;
+
+
+
+}) 
+
+
+$('.next').on('click', 'button', function() {
+	if(questionNumber < questionSet.length) {
+		console.log('keep going')
+
+		/* clear check boxes */
+		$("input:checked").removeAttr("checked");
+		
+		/* question 1 questions and answers */
+		$(this).closest('.container').find('.questionGeneral').find('.question').text(questionSet[questionNumber].question);
+		$(this).closest('.container').find('.questionGeneral').find('label[for=answer1]').text(questionSet[questionNumber].answer1);
+		$(this).closest('.container').find('.questionGeneral').find('label[for=answer2]').text(questionSet[questionNumber].answer2);
+		$(this).closest('.container').find('.questionGeneral').find('label[for=answer3]').text(questionSet[questionNumber].answer3);
+		$(this).closest('.container').find('.questionGeneral').find('label[for=answer4]').text(questionSet[questionNumber].answer4);
+		$(this).closest('.container').find('.questionGeneral').find('label[for=answer5]').text(questionSet[questionNumber].answer5);
+		
+		
+		/* hide next button */
+		$(this).closest('h2').hide();
+
+
+
+		/* hide correct and incorrect buttons */
+		$(this).closest('.bottom').find('.correct').hide();
+		$(this).closest('.bottom').find('.incorrect').hide();
+
+		/* Display submit button */
+		$(this).closest('.container').find('.submit').show();
+
+		/* increment question number*
+		questionNumber++; */
+		questionNumberDisplay ++;
+
+		console.log(questionNumberDisplay)
+
+		
+		/* add question number to questionNumber text */
+	$(this).closest('.container').find('.questionGeneral').find('.questionNumber').text(questionNumberDisplay + '/5');
+	}
+
+	else {
+		console.log('show final score')
+
+		/* hide question section */
+		$(this).closest('.questionGeneral').hide();
+
+		$(this).closest('.container').find('.endGame').find('.main').find('h2').text("You score " + userScore + " out of 5!");
+
+		if(userScore == 5) {
+			$(this).closest('.container').find('.endGame').find('.top').find('h2').text("You are a local!");
+		}
+		else if (3 < userScore && userScore < 5 ) {
+			$(this).closest('.container').find('.endGame').find('.top').find('h2').text("Are you a transplant?");
+		}
+
+		else {
+			$(this).closest('.container').find('.endGame').find('.top').find('h2').text("Keep practing buddy.");
+		}
+
+		/* display endGame section */
+		$(this).closest('.container').find('.endGame').show();
+
+	}
+
+	
 
 })
-
-
-
 
 
 
